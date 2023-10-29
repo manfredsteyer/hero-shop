3bb7c8a6831a549e6fa2b0993d391eabb2b7f634
Manfred Steyer <manfred.steyer@gmx.net>
2023-10-30 00:59:34 +0100

    showcase transition api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -7,6 +7,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(
     routes,
-    withComponentInputBinding()
+    withComponentInputBinding(),
+    withViewTransitions(),
   ), provideClientHydration()]
 };
